617750042eac4b6572bfa094f25abaa13831500b
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-03-09 03:32:05 +0900

    feat: Ubuntu版pipのインストール処理追加

--- main-linux.fish
+++ main-linux.fish
@@ -9,6 +9,7 @@ sudo apt-get -y install build-essential
 sudo apt-get -y install cmake
 sudo apt-get -y install libssl-dev
 sudo apt-get -y install mingw-w64
+sudo apt-get -y install python3-pip
 
 # Lazygit のインストール
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_0.40.2_Linux_x86_64.tar.gz"
