dfebf4fbdb2971ab64083987aa9e235306867a42
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2024-03-09 03:21:50 +0900

    feat: 設定ファイルのディレクトリを作成する処理の追加

--- fish-linux.sh
+++ fish-linux.sh
@@ -2,6 +2,7 @@
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
 sudo apt -y install fish
+mkdir ~/.config
 cp -f config.fish ~/.config/fish/config.fish
 which fish
 echo "which fish で表示されたパスを入力して下さい。"
